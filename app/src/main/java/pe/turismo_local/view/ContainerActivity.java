9df96a9f272e5c18fb5dd73b422bcca5691d73f3
OverCode <avalosfigueroapedro@gmail.com>
2025-03-15 20:13:11 -0500

    add ContainerActivity and layout files for navigation structure

---
+++ app/src/main/java/pe/turismo_local/view/ContainerActivity.java
@@ -0,0 +1,64 @@
+package pe.turismo_local.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import pe.turismo_local.R;
+import pe.turismo_local.databinding.ActivityContainerBinding;
+import pe.turismo_local.view.fragments.FavoritesFragment;
+import pe.turismo_local.view.fragments.HomeFragment;
+import pe.turismo_local.view.fragments.LocationFragment;
+
+public class ContainerActivity extends AppCompatActivity {
+    private ActivityContainerBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityContainerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            return insets;
+        });
+
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            }
+
+            if (id == R.id.nav_locations) {
+                selectedFragment = new LocationFragment();
+            }
+
+            if (id == R.id.nav_favorites) {
+                selectedFragment = new FavoritesFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
+    }
+}
--- app/src/main/java/pe/turismo_local/view/MainActivity.java
+++ app/src/main/java/pe/turismo_local/view/MainActivity.java
@@ -1,64 +1,40 @@
 package pe.turismo_local.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
 
 import pe.turismo_local.R;
-import pe.turismo_local.databinding.ActivityMainBinding;
-import pe.turismo_local.view.fragments.FavoritesFragment;
-import pe.turismo_local.view.fragments.HomeFragment;
-import pe.turismo_local.view.fragments.LocationFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+
+    Button btnInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new HomeFragment())
-                .commit();
+        btnInicio = findViewById(R.id.btnInicio);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
 
-        binding.bottomNav.setOnItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
-            int id = item.getItemId();
+    public void changeActivity(View view) {
+        Intent intent = new Intent(this, ContainerActivity.class);
 
-            if (id == R.id.nav_home) {
-                selectedFragment = new HomeFragment();
-            }
-
-            if (id == R.id.nav_locations) {
-                selectedFragment = new LocationFragment();
-            }
-
-            if (id == R.id.nav_favorites) {
-                selectedFragment = new FavoritesFragment();
-            }
-
-            if (selectedFragment != null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, selectedFragment)
-                        .commit();
-            }
-
-            return true;
-        });
+        startActivity(intent);
     }
 }
