966c462794cf3a19165cafceb46af3f4bfdc0a5e
OverCode <avalosfigueroapedro@gmail.com>
2025-03-16 16:10:58 -0500

    refactor DatabaseHelper by removing unused imports and cleaning up code

--- app/src/main/java/pe/turismo_local/model/DatabaseHelper.java
+++ app/src/main/java/pe/turismo_local/model/DatabaseHelper.java
@@ -1,15 +1,10 @@
 package pe.turismo_local.model;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "lugares.db";
